d6684ba96c24adcdb600b9a0b4f2d35ef88911dd
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-03-16 19:41:16 +0000

    Added simple form - needs params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,14 @@ class PagesController < ApplicationController
   def home
     @tasks = Task.all.order('due_date DESC, importance')
   end
+
+  def create
+    @task = Task.new()
+  end
+
+  private
+
+  def task_params
+    # add parameters
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,11 @@
 class Task < ApplicationRecord
   has_many :subtasks
 
+  # callbacks
   after_initialize :set_task_content_original
+  after_commit :update_task_log
+
+  # validation
   validates :name, presence: true, uniqueness: true, length: { in: 10..150 }
   validates :importance, numericality: { less_than: 4 }
   validates :difficulty, numericality: { less_than: 4 }
@@ -19,5 +23,16 @@ class Task < ApplicationRecord
     return if due_date.nil?
 
     errors.add(:due_date.nil, "can't be in the past") unless due_date >= Date.today
+  end
+
+  def update_task_log
+    subtasks = []
+    File.open(Rails.public_path.join('task_log.txt'), 'w') do |file|
+      Task.all.order('due_date DESC, importance').each do |record|
+        subtasks << record.subtasks.each(&:name)
+        # file.write("#{record.name}, #{record.reason}, by #{record.due_date}\n#{subtasks.join('\n-') if subtasks.any?}")
+        file.write("#{record.name}, #{record.reason}, by #{record.due_date}\n")
+      end
+    end
   end
 end
