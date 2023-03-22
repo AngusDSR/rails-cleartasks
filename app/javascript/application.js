3a5dc20d4030b47fa98c6ec55c0739a1c3129c67
magdalena krasicka <krasickamagdalena@gmail.com>
2023-03-22 19:44:12 +0000

    javascript functiom

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,15 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+const tasks = document.querySelectorAll('.note-text')
+
+function openTask(e) {
+  console.log(e);
+}
+
+tasks.forEach((task) => {
+  task.addEventListener("click", openTask(e));
+});
+
+console.log("hello");
