978289c8d7ec45b1ae79770e598598aad614b5e3
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-04-07 10:49:30 +0100

    Separate lists of new tasks and detailed tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,12 @@ class TasksController < ApplicationController
   def index
     # notes
     # filter the tasks based on what filled are empty
-    @new_tasks = Task.where.not(content: nil).where(name: [nil, ''], due_date: [nil, ''], reason: [nil, ''])
-    @new_task = Task.new
-    #
-    @tasks = Task.all.order(created_at: :desc)
 
+    @new_tasks = Task.where.not(content: nil).where(reason: [nil, '']).order(created_at: :desc)
+    .or(Task.where.not(content: nil).where(due_date: [nil, ''])).order(created_at: :desc)
+    .or(Task.where.not(content: nil).where(name: [nil, ''])).order(created_at: :desc)
+    @new_task = Task.new
+    @tasks = Task.where.not(content: '', name: '', due_date: '', reason: '')
   end
 
   def create
@@ -41,6 +42,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:content, :reason, :due_date, :importance, :difficulty, :contact )
+    params.require(:task).permit(:content, :name, :reason, :due_date, :importance, :difficulty, :contact )
   end
 end
