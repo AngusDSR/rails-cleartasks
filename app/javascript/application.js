08ca9cee6cda02839c43e0257bd00178155af8c5
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-04-10 15:57:07 +0100

    Changed layout, added basic styling and allowed submit with keyboard

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,13 +3,23 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 
-
 // move this to separate file and import
+const newNote = document.getElementById('new_task');
 const tasks = document.querySelectorAll('.note-text');
 const closeTaskBtn = document.querySelectorAll('.btn-close');
 const taskViewBtns = document.querySelectorAll('.btn-view-task-details');
 const noteTexts = document.querySelectorAll('.note-text');
 const CloseUpdateTaskBtns = document.querySelectorAll('.btn-close-task-update');
+
+window.addEventListener('keydown', function(e) {
+  console.log(e.key === 'Enter' && e.ctrlKey)
+    if (e.key === 'Enter' && e.ctrlKey) {
+    e.preventDefault();
+    console.log(newNote);
+    console.dir(newNote);
+    newNote.submit();
+  }
+});
 
 tasks.forEach((task) => {
   task.addEventListener("click", function(e) {
