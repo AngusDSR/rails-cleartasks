9be1bfe0e3641785b337b9641936e3fc7677b2d0
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-05-08 19:54:48 +0100

    subtasks can be added and seen in the notes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: "tasks#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :tasks do
-    resources :subtasks, only: %i[new update destroy]
+    resources :subtasks, only: %i[create update destroy]
   end
 end
