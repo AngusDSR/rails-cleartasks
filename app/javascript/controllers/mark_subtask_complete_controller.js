3218f37d95b012471600b02bc69b192107da183e
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-05-10 19:48:18 +0100

    Controller for subtask connected - only

--- app/javascript/controllers/mark_subtask_complete_controller.js
+++ app/javascript/controllers/mark_subtask_complete_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="mark-subtask-complete"
 export default class extends Controller {
   connect() {
+    console.log("I'm connected - sub task")
   }
 }
