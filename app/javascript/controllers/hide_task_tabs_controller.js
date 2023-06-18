0114a815bc08dfe13f4aee5f4a032a83518543c4
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-06-18 18:57:12 +0100

    hide/show title works - need to add flex so currently displaying task fills sspace with buttons at bottom

--- app/javascript/controllers/hide_task_tabs_controller.js
+++ app/javascript/controllers/hide_task_tabs_controller.js
@@ -4,14 +4,33 @@ export default class extends Controller {
   static targets = [ "tasksTitle", "taskTabButton" ]
 
   connect() {
-    console.log('I am here8')
+    console.log('I am here5')
   }
 
-  test(e) {
-    this.tasksTitleTarget.innerText = "Clicked"
-    // when I click a tab title, it should:
-    // hide all other tabs
+  highlightCurrentTask(e) {
+    let thisTab = this.taskTabButtonTargets[e.currentTarget.id]
+    let taskBody = thisTab.parentElement.nextElementSibling
+
+    // hide/show the title
+    if (this.tasksTitleTarget.style.display != "none") {
+      this.tasksTitleTarget.style = "display: none;";
+    } else {
+      this.tasksTitleTarget.style = "display: block;"
+    }
+
+    // toggle the style of the current tab to be a title
+    thisTab.classList.toggle('section-title');
+    thisTab.classList.toggle('task-as-title');
+
+    // toggle display of all other tabs
+    this.taskTabButtonTargets.forEach(button => {
+      if (button !== thisTab) {
+        button.classList.toggle('d-none')
+      }
+    });
+
     // ensure that the current task has full height
-    // set the title of the section to the name of the task
+    console.log(taskBody)
+    console.dir(taskBody)
   }
 }
