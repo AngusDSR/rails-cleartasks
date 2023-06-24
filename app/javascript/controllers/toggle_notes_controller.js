210cd1ba1697ecd70ec907abfa19fb99b01c12ac
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-06-24 17:26:46 +0100

    toggle notes view

--- app/javascript/controllers/toggle_notes_controller.js
+++ app/javascript/controllers/toggle_notes_controller.js
@@ -2,6 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-notes"
 export default class extends Controller {
+  static targets = ['noteDetails', 'noteTitle', 'closeNoteBtn'];
+
   connect() {
   }
-}
+
+  toggleNoteDetails(e) {
+    e.preventDefault();
+    this.noteDetailsTarget.classList.toggle('hidden')
+    this.noteTitleTargets.forEach((noteTitle) => {
+      noteTitle.classList.toggle('hidden')
+    });
+  };
+};
