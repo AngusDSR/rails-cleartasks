d5405a6b7f23c64bd436f6d23e1fbbfb5ec90fa7
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-08-16 20:46:50 +0100

    Issue solved:user able to add new notes after delete

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,14 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-
-// move this to separate file and import
-const newNoteForm = document.getElementById('new_task');
-
-// Submit form and save notes
-window.addEventListener('keydown', function(e) {
-  e.preventDefault();
-    if (e.key === 'Enter' && e.ctrlKey) {
-      newNoteForm.submit();
-  }
-});
--- app/javascript/controllers/new_note_form_submit_controller.js
+++ app/javascript/controllers/new_note_form_submit_controller.js
@@ -1,21 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller, add } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-note-form-submit"
 export default class extends Controller {
   static targets = [ "form" ]
   connect() {
-    console.log('I am connected');
-    console.log(this.formTarget);
+    this.addEventListenerToWindow();
+  }
+
+  addEventListenerToWindow() {
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter' && e.ctrlKey) {
+        e.preventDefault();
+        this.formTarget.submit();
+      }
+    });
   }
 }
-
-
-const newNoteForm = document.getElementById('new_task');
-
-// Submit form and save notes
-window.addEventListener('keydown', function(e) {
-    if (e.key === 'Enter' && e.ctrlKey) {
-      e.preventDefault();
-      newNoteForm.submit();
-  }
-});
